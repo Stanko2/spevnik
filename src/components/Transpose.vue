97f9458e2d53f30e31afc0949f55545ed1bff0a6
Stanko2 <stanko@bezak.net>
2022-09-10 23:39:23 +0200

    Reset transpose on song change

--- src/components/Transpose.vue
+++ src/components/Transpose.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="fixed bottom-14 right-0 dark:bg-gray-700 dark: dark:text-gray-100 p-2 rounded-t-md flex items-center shadow-md z-20 bg-gray-300 text-gray-900">
         <button @click="changeScale" class="p-1 font-bold hover:bg-gray-500 rounded-md mx-1 transition-all w-8">{{currentScale}}</button>
-        <span>{{ scale[currentScale][currentTranspose]}}</span>
+        <span>{{ currentTranspose }}</span>
         <button class="flex items-center justify-center p-1 hover:bg-gray-500 rounded-md mx-1 transition-all" @click="transpose(-1)"><span class="material-symbols-rounded block">expand_more</span></button>
         <button class="flex items-center justify-center p-1 hover:bg-gray-500 rounded-md mx-1 transition-all" @click="transpose(1)"><span class="material-symbols-rounded block">expand_less</span></button>
     </div>
@@ -10,6 +10,7 @@
 <script lang="ts">
 import Vue from 'vue'
 import Component from 'vue-class-component'
+import { Watch } from 'vue-property-decorator'
 
 @Component
 export default class Transposer extends Vue {
@@ -36,6 +37,12 @@ export default class Transposer extends Vue {
     changeScale (): void {
       this.currentScale = this.currentScale === '#' ? 'b' : '#'
       this.transpose(0)
+    }
+
+    @Watch('$route.params.id')
+    reset (): void {
+      this.currentScale = '#'
+      this.currentTranspose = 0
     }
 }
 </script>
